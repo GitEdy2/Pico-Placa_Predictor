2c17e29c5a37f13e0cc1538998229088ecb8c5ea
Edison <clanuzumaki22@gmail.com>
2022-04-29 11:58:26 -0500

    Creacion de clases y logica de aplicacion.

--- resources/index.js
+++ resources/index.js
@@ -33,3 +33,65 @@ class Plate {
 
     }
 }
+
+
+class USER_INTERFACE {
+    showAlert(message, name) {
+        document.getElementById(name).innerText=message;
+
+        setTimeout(function() {
+            document.querySelector('#err_message').innerText='';
+        }, 3000);
+    }
+
+    plateValidation(plate){
+        let exp = new RegExp(/^[a-z]{3,3}-\d{4,4}$/);
+        return exp.test(plate);
+    }
+
+    cleanInfoMessage() {
+        document.querySelector('#info_message').innerText='';
+    }
+}
+
+
+document.getElementById('plate_form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const ui = new USER_INTERFACE();
+
+    const plate = document.getElementById('plate').value.toLowerCase().trim();
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+
+
+    if(!ui.plateValidation(plate)){
+        ui.showAlert('Inserte una placa valida, ejem: ABC-1234', 'error');
+    }
+    else {
+        if(plate === '' || date === '' || time === '') {
+            ui.showAlert('Por favor, llenar todos los campos', 'error');
+        }
+        else {
+            const plateObj = new Plate(plate,date,time);
+            const msg = document.getElementById('info_message');
+
+            plateObj.infoSubmited((e,i) => {
+                if(e){
+                    msg.style.color = 'green';
+                    msg.innerText = i;
+                }
+                else {
+                    msg.style.color = 'yellow';
+                    msg.innerText = i;
+                }
+            });
+
+            setTimeout(function() {
+                ui.cleanInfoMessage();
+            }, 4000);
+
+        }
+    }
+
+});
