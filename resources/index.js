06b96aca60d7be60cea4b885d53e21c71a7e2bcb
Edison <clanuzumaki22@gmail.com>
2022-04-30 15:13:28 -0500

    Comentarios de procesos y manejo de errores.

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,5 @@
+
+//Creacion de clase para Placa.
 class Plate {
     constructor(plate,date,hour){
         this.plate = plate.toString();
@@ -5,8 +7,9 @@ class Plate {
         this.time = hour.toString();
     }
 
-
     infoSubmited(callback) {
+        
+//Obtener valores para el proceso de validacion de placa.
 
         const dateDay = new Date(`${this.date.replace(/-/g,'/')},${this.time}`);
         const plateLastDigit = parseInt(this.plate.charAt(this.plate.length-1));
@@ -14,19 +17,20 @@ class Plate {
         const hourDay = dateDay.getHours();
         const minHour = dateDay.getMinutes();
 
-
         const rest = RESTRICTIONS[day].plate_lastDigit;
 
 
+//Logica para identificar el ultimo digito de la placa.
+
         if(rest.includes(plateLastDigit)) {
             if((hourDay>=7 && hourDay<=9) || (hourDay>=16 && hourDay<=19)) {
-               if((hourDay===9 || hourDay===19) && (minHour>30)) {
+               if((hourDay===9 || hourDay===19) && (minHour>=30)) {
                    return callback (true, `Puede circular`);
                }
                return callback(false, `No puede circular`);
             }
             else {
-                return callback(false, `Puede circular`);
+                return callback(true, `Puede circular`);
             }
         }
         return callback(true, `Puede circular`);
@@ -34,10 +38,10 @@ class Plate {
     }
 }
 
-
+//Clase que maneja la funciones de la interfaz de usuario.
 class USER_INTERFACE {
     showAlert(message, name) {
-        document.getElementById(name).innerText=message;
+        document.getElementById(name).innerText = message;
 
         setTimeout(function() {
             document.querySelector('#err_message').innerText='';
@@ -58,6 +62,9 @@ class USER_INTERFACE {
 document.getElementById('plate_form').addEventListener('submit', function(e) {
     e.preventDefault();
 
+
+    //Creacion de instacia para interfaz con el usuario.
+
     const ui = new USER_INTERFACE();
 
     const plate = document.getElementById('plate').value.toLowerCase().trim();
@@ -66,30 +73,30 @@ document.getElementById('plate_form').addEventListener('submit', function(e) {
 
 
     if(!ui.plateValidation(plate)){
-        ui.showAlert('Inserte una placa valida, ejem: ABC-1234', 'error');
+        ui.showAlert('Inserte una placa valida, ejem: ABC-1234', 'err_message');
     }
     else {
-        if(plate === '' || date === '' || time === '') {
-            ui.showAlert('Por favor, llenar todos los campos', 'error');
+        if((plate == '' || date == '' || time == '')) {
+            ui.showAlert('Por favor, llenar todos los campos', 'err_message');
         }
         else {
-            const plateObj = new Plate(plate,date,time);
+            const objPlate = new Plate(plate,date,time);
             const msg = document.getElementById('info_message');
 
-            plateObj.infoSubmited((e,i) => {
+            objPlate.infoSubmited((e,i) => {
                 if(e){
                     msg.style.color = 'green';
                     msg.innerText = i;
                 }
                 else {
-                    msg.style.color = 'yellow';
+                    msg.style.color = 'brown';
                     msg.innerText = i;
                 }
             });
 
             setTimeout(function() {
                 ui.cleanInfoMessage();
-            }, 4000);
+            }, 2000);
 
         }
     }
